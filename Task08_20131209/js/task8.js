d4acfb476304a76e797a697149e5dac2d512ec2d
Mykola Grybyk <xlenz7@gmail.com>
2013-12-20 02:48:44 +0200

    task8: accord, colors
    no drag

--- Task08_20131209/js/task8.js
+++ Task08_20131209/js/task8.js
@@ -1,25 +1,66 @@
-(function($) {
-  var allPanels = $('#accordion .tab-body').hide();
+'use strict';
 
-  $('#accordion .tab-head a').click(function() {
-    allPanels.slideUp();
-    $(this).parent().parent().find('.tab-body').slideDown();
-    return false;
-  });
+(function ($) {
+    var tabBodies = $('#accordion .tab-body').css('max-height', 0);
 
+    $('#accordion .tab-head').click(function (e) {
+        e.preventDefault();
+        var tabBody = $(this).next('.tab-body');
+
+        if (tabBody.height() > 0) {
+            tabBody.stop().animate({height: '0'}, function () {
+                $(this).css('max-height', 0);
+            });
+        } else {
+            tabBody
+                .css('height', '')
+                .stop().animate({maxHeight: '400px'})
+                .not(tabBody).stop().animate({height: 0}, function () {
+                    $(this).css('max-height', 0);
+                });
+        }
+    });
 })(jQuery);
 
-(function($) {
-  var allPanels = $('.accordion > dd').hide();
+(function ($) {
+    $('.chameleon')
+        .hover(
+            function (e) {
+                $(this).stop().animate({backgroundColor: '#738678'});
+            },
+            function (e) {
+                $(this).stop().animate({backgroundColor: '#00FF47'});
+            }
+        )
+        .mousedown(function (e) {
+            $(this).stop().animate({backgroundColor: '#A0FFBA'});
+        })
+        .mouseup(function (e) {
+            $(this).stop().animate({backgroundColor: '#738678'});
+        });
+})(jQuery);
 
-  $('.accordion > dt > a').click(function() {
-      $this = $(this);
-      $target =  $this.parent().next();
+(function ($) {
+    //
+})(jQuery);
 
-      if(!$target.hasClass('active')){
-         allPanels.removeClass('active').slideUp();
-         $target.addClass('active').slideDown();
-      }
 
-    return false;
-  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
