987d405c7da04ef50db54801c74809ff8877c493
Mykola Grybyk <xlenz7@gmail.com>
2014-01-20 01:26:49 +0200

    task8 complete: drag

--- Task08_20131209/js/draggable.js
+++ Task08_20131209/js/draggable.js
@@ -1,5 +1,59 @@
 'use strict';
 
-(function() {
-  //
+(function () {
+  var config = {
+    dragColorClick: 'red'
+  };
+  var dragging = false;
+  var container = $('div.range');
+  var containerWidth = container.width();
+  var containerWidthDrag = containerWidth + 2;
+  var drag = container.find('span.drag');
+  var dragColor = drag.css('background-color');
+  var dragWidth = drag.width() * 0.5 - 2;
+  var containerPos = container.position().left;
+  var containerPosDrag = containerPos + dragWidth;
+  var overlayDark = $('#overlayDark');
+
+  $(document).on('mousemove', function (e) {
+    if (!dragging) {
+      return;
+    }
+    var leftOffset;
+    if (e.clientX < containerWidthDrag && e.clientX >= containerPosDrag) {
+      leftOffset = e.pageX - dragWidth;
+    } else if (e.clientX > containerWidthDrag) {
+      leftOffset = containerWidth - 2;
+    } else if (e.clientX < containerPosDrag) {
+      leftOffset = containerPos;
+    }
+    drag.offset({
+      left: leftOffset
+    });
+  });
+  drag.on('mousedown', function (e) {
+    e.preventDefault();
+    dragging = true;
+    drag.stop().animate({
+      backgroundColor: config.dragColorClick
+    });
+    overlayDark.css('z-index', 1).stop().animate({
+      opacity: 0.5
+    });
+  });
+
+  $(window).on("mouseup", function (e) {
+    dragging = false;
+    overlayDark.stop().animate({
+      opacity: 0
+    }, {
+      complete: function () {
+        overlayDark.css('z-index', -1);
+        drag.stop().animate({
+          backgroundColor: dragColor
+        });
+      }
+    });
+
+  });
 })();
