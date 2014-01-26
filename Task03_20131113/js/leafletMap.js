f7f48d587a098d4a4ee3beefd88d9c2c26723479
Mykola Grybyk <xlenz7@gmail.com>
2014-01-26 23:32:47 +0200

    task 3, map

--- Task03_20131113/js/leafletMap.js
+++ Task03_20131113/js/leafletMap.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var map = L.map('leafletMap').setView([51.505, -0.09], 13);
+var coordinatesJsCource = [50.417857, 30.517341];
+var map = L.map('leafletMap').setView(coordinatesJsCource, 17);
 
 L.tileLayer(
   'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
     maxZoom: 18
   }).addTo(map);
+
+L.marker(coordinatesJsCource).addTo(map)
+      .bindPopup("<strong>Cogniance</strong><br>JS Course here!").openPopup();
 
 var popup = L.popup();
 
