ab6e2438543d48f4d9c5818483049a4ed444f3fc
Mykola Grybyk <xlenz7@gmail.com>
2014-01-27 01:18:26 +0200

    added ymaps (awful zoom)

--- Task03_20131113/js/leafletMap.js
+++ Task03_20131113/js/leafletMap.js
@@ -1,23 +1,27 @@
 'use strict';
 
 var coordinatesJsCource = [50.417857, 30.517341];
-var map = L.map('leafletMap').setView(coordinatesJsCource, 17);
-
-L.tileLayer(
+var map = L.map('leafletMap').setView(coordinatesJsCource, 16);
+var osm = new L.tileLayer(
   'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
     maxZoom: 18
-  }).addTo(map);
+  });
+var yndx = new L.Yandex();
 
-L.marker(coordinatesJsCource).addTo(map)
-      .bindPopup("<strong>Cogniance</strong><br>JS Course here!").openPopup();
+map.addLayer(yndx);
+map.addControl(new L.Control.Layers({
+  'OSM': osm,
+  "Yandex": yndx
+}));
 
-var popup = L.popup();
+var defaultMarker = L.icon({
+  iconUrl: 'images/marker-icon.png',
+  shadowUrl: 'images/marker-shadow.png',
+  iconAnchor: [13, 41],
+  popupAnchor: [-1, -26]
+});
+L.marker(coordinatesJsCource, {
+  icon: defaultMarker
+}).addTo(map)
+  .bindPopup("<strong>Cogniance</strong><br>JS Course here!").openPopup();
 
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(map);
-}
-
-map.on('click', onMapClick);
