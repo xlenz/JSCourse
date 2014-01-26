3c1c8b035d5d17a9c681e7bfcd2a903fa8c2d221
Mykola Grybyk <xlenz7@gmail.com>
2014-01-26 17:07:17 +0200

    task3 (no icons, with defects)

---
+++ Task03_20131113/js/leafletMap.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var map = L.map('leafletMap').setView([51.505, -0.09], 13);
+
+L.tileLayer(
+  'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+  }).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
