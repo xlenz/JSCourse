c0db61fcf3679223af0aa918d7b8f543a5ec9c7f
Mykola Grybyk <xlenz7@gmail.com>
2014-02-15 20:52:22 +0200

    remove web folder, remove bootstrap

--- Task_Backbone-ajax_20140203/web/public/js/collections/users.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-UserManager.Collections.Users = Backbone.Collection.extend({
-  model: UserManager.Models.User,
-
-  initialize: function () {
-    console.log('Users collection initialized.');
-  }
-});
